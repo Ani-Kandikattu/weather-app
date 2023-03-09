4bf29ceef70dd76df8f6539de1ad6c3f065275e2
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-03-08 23:35:32 -0700

    Updated README and scripts files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather App
+
+## Description
+
+This challenge involved creating a weather app that could be used to view the weather in the city entered. It is useful for travel as it can show the forecast.
+I utilized HTML, CSS, JavaScript and a server side API in order to complete this challenge.
+This app also stores previously searched cities in local storage.
+I learned how to utilize server side apis in order to fetch data and display it to the webpage.
+https://github.com/Ani-Kandikattu/coding-quiz
--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ var weather = {
       .then((data) => this.displayWeather(data));
   },
 
-  //Function to display the weather in the webpage
   displayWeather: function (data) {
     var { name } = data;
     var { icon, description } = data.weather[0];
@@ -28,9 +27,45 @@ var weather = {
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".weather").classList.remove("loading");
+
+    // Store the searched city in local storage
+    var searches = JSON.parse(localStorage.getItem("searches")) || [];
+    if (!searches.includes(name)) {
+      searches.push(name);
+      localStorage.setItem("searches", JSON.stringify(searches));
+    }
+    this.displaySearches();
   },
+
+  displaySearches: function () {
+    var searches = JSON.parse(localStorage.getItem("searches")) || [];
+    var html = "";
+    for (var i = 0; i < searches.length; i++) {
+      html +=
+        "<li><button onclick='weather.fetchWeather(\"" +
+        searches[i] +
+        "\")'>" +
+        searches[i] +
+        "</button></li>";
+    }
+    document.querySelector(".searches").innerHTML = html;
+  },
+
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+
+  // Function to display the current date
+  displayDate: function () {
+    var date = new Date();
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    var dateString = date.toLocaleDateString("en-US", options);
+    document.querySelector(".date").innerText = dateString;
   },
 };
 
@@ -41,3 +76,6 @@ document.querySelector(".search button").addEventListener("click", function () {
 
 //Function to load weather for a default city when the page is first opened
 weather.fetchWeather("Phoenix");
+
+// Display previously searched cities on page load
+weather.displaySearches();
