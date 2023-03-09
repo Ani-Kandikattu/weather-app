934607b6354b62e5755266fb37bbbc6bbef90724
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-03-08 17:28:39 -0700

    added JS, updated CSS and HTML

--- scripts.js
+++ scripts.js
@@ -1,101 +1,43 @@
-const form = document.querySelector("form");
-const forecastDiv = document.querySelector("#forecast");
-const previousCitiesDiv = document.querySelector("#previousCities");
+//Use API to retrieve data
+var weather = {
+  apiKey: "4e952528a0af8a8992ca6e0865ad1874",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const city = form.elements.cityInput.value;
-  getForecast(city);
+  //Function to display the weather in the webpage
+  displayWeather: function (data) {
+    var { name } = data;
+    var { icon, description } = data.weather[0];
+    var { temp, humidity } = data.main;
+    var { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".temp").innerText = temp + "° F";
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".wind").innerText = "Wind: " + speed + " mph";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+//Function to get content of search bar and search
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
 });
 
-function getPreviousCities() {
-  const cities = [];
-  for (let i = 0; i < localStorage.length; i++) {
-    cities.push(localStorage.key(i));
-  }
-  return cities;
-}
-
-function createPreviousCityButton(city) {
-  const button = document.createElement("button");
-  button.textContent = city;
-  button.addEventListener("click", () => {
-    const savedForecast = getSavedForecast(city);
-    if (savedForecast) {
-      showForecast(savedForecast);
-    } else {
-      forecastDiv.innerHTML = "<p>No forecast data found for this city.</p>";
-    }
-  });
-  return button;
-}
-
-function showPreviousCities() {
-  previousCitiesDiv.innerHTML = "";
-  const cities = getPreviousCities();
-  cities.forEach((city) => {
-    const button = createPreviousCityButton(city);
-    previousCitiesDiv.appendChild(button);
-  });
-}
-
-async function getForecast(city) {
-  const apiKey = "4e952528a0af8a8992ca6e0865ad1874";
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    showForecast(data);
-    saveForecast(city, data);
-    showPreviousCities();
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-    const savedForecast = getSavedForecast(city);
-    if (savedForecast) {
-      showForecast(savedForecast);
-    } else {
-      forecastDiv.innerHTML =
-        "<p>There was a problem getting the forecast for this city.</p>";
-    }
-  }
-}
-
-function showForecast(data) {
-  const forecastArray = data.list.filter((item) =>
-    item.dt_txt.includes("12:00:00")
-  );
-  forecastDiv.innerHTML = "";
-  forecastArray.forEach((item) => {
-    const date = new Date(item.dt_txt).toLocaleDateString();
-    const temp = item.main.temp;
-    const description = item.weather[0].description;
-    const iconUrl = `https://openweathermap.org/img/w/${item.weather[0].icon}.png`;
-    const forecastItem = `
-			<div>
-				<p>${date}</p>
-				<p>${temp}&deg;C</p>
-				<p>${description}</p>
-				<img src="${iconUrl}" alt="${description}">
-			</div>
-		`;
-    forecastDiv.insertAdjacentHTML("beforeend", forecastItem);
-  });
-}
-
-function saveForecast(city, data) {
-  const forecastString = JSON.stringify(data);
-  localStorage.setItem(city, forecastString);
-}
-
-function getSavedForecast(city) {
-  const forecastString = localStorage.getItem(city);
-  if (forecastString) {
-    const forecastData = JSON.parse(forecastString);
-    return forecastData;
-  } else {
-    return null;
-  }
-}
+//Function to load weather for a default city when the page is first opened
+weather.fetchWeather("Phoenix");
